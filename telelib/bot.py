2a996965fc9b012487b087c19f8a206a31e752f9
Mohammad Mahdi Afshar <22727144+reloadlife@users.noreply.github.com>
2022-05-28 17:39:44 +0000

    Added Python Base

---
+++ telelib/bot.py
@@ -0,0 +1,25 @@
+import requests
+
+class Bot:
+	def __init__(self, token, url="https://api.telegram.org/", timeout=5, proxy=None):
+		self.__timeout = timeout
+		self.__proxy = proxy
+		self.__request = requests.Session()
+		self.__request.headers = {
+			"User-Agent": "TeleLib/6.0 (Python)",
+		}
+		self.__token = token
+		self.__url = url
+		self.__r_url = url + "bot" + token + "/"
+
+
+		self.__threads = []
+		self.__status = "stopped"
+
+	def start(self):
+		self.__status = "running"
+		...
+
+	def stop(self):
+		self.__status = "stopped"
+		...
