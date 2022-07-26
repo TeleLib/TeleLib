78b610e4198ee9e39d4a7fb899946078e22563f9
Mohammad Mahdi Afshar <me@mamad.dev>
2022-07-26 14:28:17 +0430

    Added Example Code

--- telelib.py
+++ telelib.py
@@ -3,26 +3,26 @@ from telelib import TeleLib, telegram
 if __name__ == "__main__":
     telelib = TeleLib(open("TOKEN").read().strip())
 
-    # async def main():
-    #     # await TeleLib.tg(
-    #     #     res := telegram.sendMessage(text="Hello", chat_id=400610322)
-    #     # )
+    async def main():
+        # await TeleLib.tg(
+        #     res := telegram.sendMessage(text="Hello", chat_id=400610322)
+        # )
 
-    #     # print(res.result())
-    #     ...
+        # print(res.result())
+        ...
 
-    # async def message(msg: telegram.Message):
-    #     if msg.text == "hello":
-    #         await TeleLib.tg(
-    #             res := telegram.sendMessage(
-    #                 text="Hello : )",
-    #                 chat_id=msg.chat.id
-    #             )
-    #         )
-    #         print(res.result())
+    async def message(msg: telegram.Message):
+        if msg.text == "hello":
+            await TeleLib.tg(
+                res := telegram.sendMessage(
+                    text="Hello : )",
+                    chat_id=msg.chat.id
+                )
+            )
+            print(res.result())
 
-    # telelib.Bot.set_event_handler('main', main)
-    # telelib.Bot.set_event_handler(telegram.Message, message)
+    telelib.Bot.set_event_handler('main', main)
+    telelib.Bot.set_event_handler(telegram.Message, message)
 
-    # telelib.start()
-    telelib.Tools.CodeGeneratorRun()
+    telelib.start()
+    # telelib.Tools.CodeGeneratorRun()
